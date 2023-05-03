f95b5dd19cf69cf40113477aedbf569d81f552be
Kibov <bartek01322@gmail.com>
2023-05-03 18:31:51 +0200

    Added a demo controller for Admin

--- teamtrackbackend/src/main/java/com/teamtrack/teamtrackbackend/user/UserController.java
+++ teamtrackbackend/src/main/java/com/teamtrack/teamtrackbackend/user/UserController.java
@@ -1,26 +1,30 @@
 package com.teamtrack.teamtrackbackend.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@RequestMapping("/users")
 public class UserController {
+
     private final UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    @CrossOrigin(origins = "*")
-    @GetMapping("/users")
-    public List<User> getUsers() {
-        return (List<User>) userRepository.findAll();
-    }
-    @CrossOrigin(origins = "*")
-    @PostMapping("/users")
-    void addUser(@RequestBody User user) {
-        userRepository.save(user);
+
+    @GetMapping
+    public ResponseEntity<List<String>> getUsers() {
+        List<User> userList = userRepository.findAll();
+        List<String> userStringList = new ArrayList<>();
+        for (User user : userList) {
+            String userString = "Name: " + user.getUsername() + ", Password: " + user.getPassword() + ", Role: " + user.getRole();
+            userStringList.add(userString);
+        }
+        return ResponseEntity.ok(userStringList);
     }
 
 
